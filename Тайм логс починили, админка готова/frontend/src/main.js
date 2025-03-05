0ca4e171a923d68b2a172923e48d8f05d20fb416
ERneSt⚡️os <72334898+WoLand-Q@users.noreply.github.com>
2025-03-05 11:32:16 +0200

    Лишнее

--- Тайм логс починили, админка готова/frontend/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router/index.js';
-import './assets/styles.css'; // Tailwind и кастомные стили
-import 'animate.css'; // Подключение animate.css
-
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
